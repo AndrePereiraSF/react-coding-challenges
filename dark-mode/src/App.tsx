25fdd808f651b37e371a016742b2c84b3a82fe39
Andre Pereira <contato.andrepsf@gmail.com>
2021-05-24 21:35:14 -0300

    Update App and index with the new TS code and components

---
+++ dark-mode/src/App.tsx
@@ -0,0 +1,39 @@
+import React, { useEffect, useState } from 'react'
+import Routes from './Routes'
+import ThemeContext from './contexts/ThemeContext'
+import { ThemeProvider } from 'styled-components'
+import GlobalStyle from './styles/GlobalStyle'
+import light from './themes/light'
+import Loading from './components/Loading'
+
+export const App: React.FC = () => {
+  const getCurrentTheme = () => {
+    const currentTheme = JSON.parse(localStorage.getItem('theme') || '{}')
+    return currentTheme || light
+  }
+
+  const [theme, setTheme] = useState(getCurrentTheme)
+  const [isLoading, setIsLoading] = useState(true)
+
+  useEffect(() => {
+    localStorage.setItem('theme', JSON.stringify(theme))
+  }, [theme])
+
+  useEffect(() => {
+    setTimeout(() => {
+      setIsLoading(false)
+    }, 1500)
+  }, [])
+
+  return (
+    <ThemeContext.Provider value={{ theme, setTheme }}>
+      <ThemeProvider theme={theme}>
+        {isLoading && <Loading />}
+        <GlobalStyle />
+        <Routes />
+      </ThemeProvider>
+    </ThemeContext.Provider>
+  )
+}
+
+export default App
